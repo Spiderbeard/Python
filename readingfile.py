70b86ce72d4d1ef6fcd4d92df3ccdf6a01624a27
SpiderBear23 <nicholasstevick23@gmail.com>
2021-02-24 16:05:12 -0500

    Learning file I|O

--- readingfile.py
+++ readingfile.py
@@ -1,4 +1,15 @@
 fhandle = open("c:/Users/User/Desktop/p4e/loremipsum.txt")
 print(fhandle)
 for line in fhandle:
+    line = line.rstrip()
     print(line)
+
+fname = input("Enter the file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened ",fname)
+    quit()
+for line in fhand:
+    line = line.rstrip()
+    print(line)            
